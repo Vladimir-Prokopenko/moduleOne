677160b6c8139ca8267d11c19b6d3d0518488d02
Владимир Прокопенко <78359077+Vladimir-Prokopenko@users.noreply.github.com>
2021-09-26 23:21:30 +0300

    7 ok

---
+++ js/script-1.js
@@ -0,0 +1,32 @@
+// const productName = "Droid";
+// const pricePerItem  = 2000;
+
+// let productName = "Droid";
+// productName = "Repair droid";
+// let pricePerItem = 2000;
+// pricePerItem = 3500;
+// console.log(productName);
+// console.log(pricePerItem);
+
+// const topSpeed = 160;
+// const distance = 617.54;
+// const login = "mango935";
+// const isOnline = true;
+// const isAdmin = false;
+
+// const pricePerItem = 3500;
+// const orderedQuantity = 4;
+// const totalPrice = pricePerItem * orderedQuantity;
+// console.log(totalPrice);
+
+// const productName = "Droid";
+// const pricePerItem = 3500;
+// const message = `You picked ${productName}, price per item is ${pricePerItem} credits`;
+// console.log(message);
+
+// const pricePerDroid = 800;
+// const orderedQuantity = 6;
+// const deliveryFee = 50;
+// const totalPrice = pricePerDroid * orderedQuantity + deliveryFee;
+// const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`;
+// console.log(message);
